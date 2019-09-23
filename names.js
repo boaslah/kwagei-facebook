54e75529c5c257aa84322d09f59118937eff428d
boaslah <blessing@kwagei.com>
2019-09-23 16:08:23 +0000

    updated version of v1

--- names.js
+++ names.js
@@ -1,36 +1,3 @@
-var teamMembers = [
-        {
-        firstName: "Kpetermeni",
-        lastName: "Siakor",
-        contact: "0776931461"
-        },
-        {
-        firstName: "Emanuel",
-        lastName: "Jaygbay",
-        contact: "0775715771"
-        },
-        {
-        firstName: "Nathan",
-        lastName: "Siafa",
-        contact: "0778415037"
-        },
-        {
-        firstName: "Rudeen",
-        lastName: " Zarwolo",
-        contact: "0777967844"
-        },
-        {
-        firstName: "Carlos",
-        lastName: "Nah",
-        contact: "0776728619"
-        },
-        {
-        firstName: "Blessing",
-        lastName: "Boaslah",
-        contact: "0778128252"
-        }
-];
-
 // declare variables
 var tableBody = document.getElementById("tbody");
 var addFirstNameInput = document.getElementById("addFirstNameInput");
@@ -47,133 +14,193 @@ var table = document.getElementById("tablediv");
 var deleteInput = document.getElementById("deleteInput");
 
 
-// function to show the  add staff input elements
-function showAddStaffInputElements() {
-        deleteStaffInput.style.display = "none";
-        addStaffInputs.style.display = "block";
-        addStaffInputs.style.display = "flex";
-        editStaffInputs.style.display = "none";
-}
-
-// function to show the  edit staff input elements
-function showEditStaffInputElements() {
-        addStaffInputs.style.display = "none";
-        deleteStaffInput.style.display = "none";
-        editStaffInputs.style.display = "block";
-        editStaffInputs.style.display = "flex";
-}
-
-// function to show the  delect staff input element
-function showDeleteStaffInputElement() {
-        deleteStaffInput.style.display = "block";
-        addStaffInputs.style.display = "none";
-        editStaffInputs.style.display = "none";
-        deleteStaffInput.style.display = "flex";
-}
-
-displayStaff();
-
-// function to display staff
-function displayStaff() {
-        table.style.display = " block";
-
-        for (i = 0; i < teamMembers.length; i++) {
-
-                // creat table data elements
-                var rowElement = document.createElement("tr");
-                var numberElement = document.createElement("td");
-                var firstNameElement = document.createElement("td");
-                var lastNameElement = document.createElement("td");
-                var contactElement = document.createElement("td");
-        
-                // creat text content for table data elements
-                numberElement.textContent = i + 1;
-                firstNameElement.textContent = teamMembers[i].firstName;
-                lastNameElement.textContent = teamMembers[i].lastName;
-                contactElement.textContent = teamMembers[i].contact;
-        
-                // append table data to table row
-                rowElement.append(numberElement);
-                rowElement.append(firstNameElement);
-                rowElement.append(lastNameElement);
-                rowElement.append(contactElement);
-        
-                // appemd table row to table body
-                tableBody.append(rowElement);
-        }
-};
-
-
-
-// function to add staff
-function addStaff() {
-
-        var newTeamMembers = {
-                firstName: addFirstNameInput.value,
-                lastName: addLastNameInput.value,
-                contact: addContactInput.value
-        };
-
-        //creat table data elements
-        var rowElement = document.createElement("tr");
-        var numberElement = document.createElement("td");
-        var firstNameElement = document.createElement("td");
-        var lastNameElement = document.createElement("td");
-        var contactElement = document.createElement("td");
-
-        // creat text content for table data elements
-        firstNameElement.textContent = newTeamMembers.firstName;
-        lastNameElement.textContent = newTeamMembers.lastName;
-        contactElement.textContent = newTeamMembers.contact;
-        numberElement.textContent = teamMembers.length+1;
-
-
-        // append table data to table row
-        rowElement.append(numberElement);
-        rowElement.append(firstNameElement);
-        rowElement.append(lastNameElement);
-        rowElement.append(contactElement);
-
-        // appemd table row to table body
-        tableBody.append(rowElement);
-
-        addFirstNameInput.value = " ";
-        addLastNameInput.value = " ";
-        addContactInput.value = " ";
-}
-
-
-
-// function to edit staff
-function editStaff() {
-
-        for (i = 0; i < teamMembers.length; i++) {
-                if (noInput.value - 1 == i) {
-                        teamMembers[i].firstName = editFirstNameInput.value;
-                        teamMembers[i].lastName = editLastNameInput.value;
-                        teamMembers[i].contact = editContactInput.value;
+//kwagei facefook objeect
+var kwageiFacebook =  {
+       teamMembers:[
+                {
+                firstName: "Kpetermeni",
+                lastName: "Siakor",
+                contact: "0776931461"
+                },
+                {
+                firstName: "Emmanuel",
+                lastName: "Jaygbay",
+                contact: "0775715771"
+                },
+                {
+                firstName: "Nathan",
+                lastName: "Siafa",
+                contact: "0778415037"
+                },
+                {
+                firstName: "Rudeen",
+                lastName: " Zarwolo",
+                contact: "0777967844"
+                },
+                {
+                firstName: "Carlos",
+                lastName: "Nah",
+                contact: "0776728619"
+                },
+                {
+                firstName: "Blessing",
+                lastName: "Boaslah",
+                contact: "0778128252"
                 }
-        }
-        tableBody.innerHTML = " ";
+        ],
+   
+        // function to show the  add staff input elements
+        showAddStaffInputElements: function () {
+                deleteStaffInput.style.display = "none";
+                addStaffInputs.style.display = "block";
+                addStaffInputs.style.display = "flex";
+                editStaffInputs.style.display = "none";
+        },
+        
+        // function to show the  edit staff input elements
+        // showEditStaffInputElements: function () {
+        //         addStaffInputs.style.display = "none";
+        //         deleteStaffInput.style.display = "none";
+        //         editStaffInputs.style.display = "block";
+        //         editStaffInputs.style.display = "flex";
+        // },
+        
+        // function to show the  delect staff input element
+        // showDeleteStaffInputElement: function () {
+        //         deleteStaffInput.style.display = "block";
+        //         addStaffInputs.style.display = "none";
+        //         editStaffInputs.style.display = "none";
+        //         deleteStaffInput.style.display = "flex";
+        // },
+        
+        // function to display staff
+        displayStaff: function () {
 
-        displayStaff();
-
-        editFirstsNameInput.value = " ";
-        editLastNameInput.value = " ";
-        editContactInput.value = " ";
-        noInput.value = " ";
-};
-
-// function to delete satff
-function deleteStaff() {
-        for (i = 0; i < teamMembers.length; i++) {
-                if (deleteInput.value - 1 == i) {
-                        teamMembers.splice(i, 1);
+                for (i = 0; i < this.teamMembers.length; i++) {
+        
+                        // creat table data elements
+                        var rowElement = document.createElement("tr");
+                        var numberElement = document.createElement("td");
+                        var firstNameElement = document.createElement("td");
+                        var lastNameElement = document.createElement("td");
+                        var contactElement = document.createElement("td");
+                        var editAction = document.createElement("td");
+                        var deleteAction = document.createElement("td");
+                        deleteAction = document.createElement("button");
+                        editAction = document.createElement("button");
+        
+                        // creat text content for table data elements
+                        deleteAction.textContent = "Delete";
+                        editAction.textContent = "Edit"
+                        numberElement.textContent = i + 1;
+                        firstNameElement.textContent = this.teamMembers[i].firstName;
+                        lastNameElement.textContent = this.teamMembers[i].lastName;
+                        contactElement.textContent = this.teamMembers[i].contact;
+        
+                        // append table data to table row
+                        rowElement.append(numberElement);
+                        rowElement.append(firstNameElement);
+                        rowElement.append(lastNameElement);
+                        rowElement.append(contactElement);
+                        rowElement.append(deleteAction);
+                        rowElement.append(editAction);
+                
+                        // appemd table row to table body
+                        tableBody.append(rowElement);
                 }
+        },
+
+      
+        
+        // function to add staff
+        addStaff: function () {
+                console.log(addFirstNameInput.value !== '')
+                if(addFirstNameInput.value !== " " && addLastNameInput.value !== "  " && addContactInput.value !== "  " ){
+
+                        var newTeamMembers = {
+                                firstName: addFirstNameInput.value,
+                                lastName: addLastNameInput.value,
+                                contact: addContactInput.value
+                        };
+
+                        //creat table data elements
+                        var rowElement = document.createElement("tr");
+                        var numberElement = document.createElement("td");
+                        var firstNameElement = document.createElement("td");
+                        var rowElement = document.createElement("tr");
+                        var numberElement = document.createElement("td");
+                        var firstNameElement = document.createElement("td");
+                        var lastNameElement = document.createElement("td");
+                        var contactElement = document.createElement("td");
+                        var lastNameElement = document.createElement("td");
+                        var contactElement = document.createElement("td");
+                        var editAction = document.createElement("td");
+                        var deleteAction = document.createElement("td");
+                        deleteAction = document.createElement("button");
+                        editAction = document.createElement("button");
+
+                        // creat text content for table data elements
+                        deleteAction.textContent = "Delete";
+                        editAction.textContent = "Edit"
+                        firstNameElement.textContent = newTeamMembers.firstName;
+                        lastNameElement.textContent =  newTeamMembers.lastName;
+                        contactElement.textContent = newTeamMembers.contact;
+                        numberElement.textContent = this.teamMembers.length + 1;
+                        this.teamMembers.push(newTeamMembers)
+
+                        // append table data to table row //creat table data elements
+                        rowElement.append(numberElement);
+                        rowElement.append(firstNameElement);
+                        rowElement.append(lastNameElement);
+                        rowElement.append(contactElement);
+                        rowElement.append(deleteAction);
+                        rowElement.append(editAction);
+
+                        // appemd table row to table body
+                        tableBody.append(rowElement);
+
+                        // clear input elements
+                        addFirstNameInput.value = " ";
+                        addLastNameInput.value = " ";
+                        addContactInput.value = " ";
+                }else{
+                        alert("please enter a user")
+                }
+        },
+        
+        // function to edit staff
+        editStaff:  function () {
+        
+                for (i = 0; i < this.teamMembers.length; i++) {
+                        if (noInput.value - 1 == i) {
+                                this.teamMembers[i].firstName = editFirstNameInput.value;
+                                this.teamMembers[i].lastName = editLastNameInput.value;
+                                this.teamMembers[i].contact = editContactInput.value;
+                        }
+                }
+                tableBody.innerHTML = " ";
+                //creat table data elements
+                this.displayStaff();
+        
+                editFirstNameInput.value = " ";
+                editLastNameInput.value = " ";
+                editContactInput.value = " ";
+                noInput.value = " ";
+        },
+        
+        // function to delete satff
+        deleteStaff: function () {
+                for (i = 0; i < this.teamMembers.length; i++) {
+                        if (deleteInput.value - 1 == i) {
+                                this.teamMembers.splice(i, 1);
+                        }
+                }
+                tableBody.innerHTML = " ";
+        
+                this.displayStaff();
+        
+                deleteInput.value  = " ";
         }
-        tableBody.innerHTML = " ";
-
-        displayStaff();
-
-        deleteInput.value  = " ";
 }
+
+kwageiFacebook.displayStaff();
